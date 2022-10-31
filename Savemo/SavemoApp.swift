9bd74c12f450e4e5339dfb2c75eba91e49694b75
Thiago <thiagoassis.99@gmail.com>
2022-10-30 22:45:52 -0300

    Initial Commit

---
+++ Savemo/SavemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavemoApp.swift
+//  Savemo
+//
+//  Created by Thiago Assis on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SavemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

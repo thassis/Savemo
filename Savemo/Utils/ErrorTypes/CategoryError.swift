b748df5ebd28d1a4c26025a7e95bba01dad44ed4
Thiago de Assis <thiagoassis.99@gmail.com>
2022-11-07 20:57:26 -0300

    feat: tests implementation (without code yet)

---
+++ Savemo/Utils/ErrorTypes/CategoryError.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryError.swift
+//  Savemo
+//
+//  Created by Thiago Assis on 07/11/22.
+//
+
+import Foundation
+
+enum CategoryError: Error {
+    case nameCannotBeZero
+}
---
+++ Savemo/Utils/Validations/OperationInput.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  Savemo
+//
+//  Created by Thiago Assis on 07/11/22.
+//
+
+struct OperationInput {
+  
+  static func validateValue(_ value: String) {
+
+  }
+
+  static func validateDate(_ date: String) {
+
+  }
+
+  static func validateCategory(_ categories: [Category], _ name: String) {
+
+  }
+
+}
